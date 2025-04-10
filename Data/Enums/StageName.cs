51fc548a97b776e47007e21aa3326ac87aa11f88
NikoCevid <niko.cevid10@gmail.com>
2025-04-10 17:53:25 +0200

    Data objects

---
+++ Data/Enums/StageName.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Enums
+{
+    public enum StageName { Final, FirstStage, PlayOffForThirdPlace, QuarterFinals, RoundOf16, SemiFinals };
+
+}
